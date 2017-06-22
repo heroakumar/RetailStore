302bccfedbf6a60def7b1a7c278d94df1613f358
AnilSharma <heroakumar@gmail.com>
2017-06-22 11:31:18 +0530

    discount service added

--- RetailStore.Core/Entity/Category.cs
+++ RetailStore.Core/Entity/Category.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Collections.Generic;
 namespace RetailStore.Core.Entity
 {
     public class Category
-    {  
+    {
+        public static readonly Category CLOTHINGS = new Category("Clothing");
+        public static readonly Category ELECTRONICS = new Category("Electronics");
+        public static readonly Category GROCERY = new Category("Grocery");
+        public static readonly Category OTHER = new Category("Other");
+
+
+
+        public Category(string name)
+        {
+            Name = name;
+        }
         public string Name { get; set; }
         public string Description { get; set; }
         public virtual ICollection<Product> Products { get; set; } 
-    }
-
+    } 
 }
--- RetailStore.Core/Entity/Role.cs
+++ RetailStore.Core/Entity/Role.cs
@@ -1,18 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace RetailStore.Core.Entity
 {
     public class Role
     {
-         
+        public static readonly Role EMPLOYEE = new Role("Employee");
+        public static readonly Role AFFILATE = new Role("Affilate");
+        public static readonly Role CUSTOMER = new Role("Customer");
+
+
+        public Role(string name)
+        {
+            Name = name;
+            RoleDesc = name;
+        }
+
+
+
         public string Name { get; set; }
         public string RoleDesc { get; set; }
         public List<User> Users { get; set; }
- 
     }
-
 }
