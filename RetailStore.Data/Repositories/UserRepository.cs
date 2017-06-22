e49c6810e3e853adcaa63ab4569d6cc0a52fbc1c
AnilSharma <akjanme@gmail.com>
2017-06-22 11:03:53 +0530

    user repository added

---
+++ RetailStore.Data/Repositories/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using RetailStore.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailStore.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private MockData data = new MockData();
+
+        public IEnumerable<User> Get()
+        {
+            return data.Users;
+        }
+
+        public User GetByID(int id)
+        {
+            return data.Users.Find(m => m.ID == id);
+        } 
+
+        public void Insert(User obj)
+        {
+            data.Users.Add(obj);
+        }
+
+        public void Update(User obj)
+        {
+            User existing = data.Users.Find(m => m.ID == obj.ID);
+            existing = obj;
+        }
+
+        public void Delete(int id)
+        {
+            User existing = data.Users.Find(m => m.ID == id);
+            data.Users.Remove(existing);
+        }
+
+        public void Save()
+        {
+            //nothing here
+        }
+    }
+
+}
