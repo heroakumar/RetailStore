4938f1e6736fee1a1ade9a1dd00a8fcf9accc60e
AnilSharma <heroakumar@gmail.com>
2017-06-22 11:47:21 +0530

    discount logic added

--- RetailStore.Services/DiscountService.cs
+++ RetailStore.Services/DiscountService.cs
@@ -11,9 +11,47 @@ namespace RetailStore.Services
 
         public DiscountModel CalculateDiscount(User user, List<Product> products)
         {
-            return new DiscountModel();
+            decimal discountPercentage = 0;
+            
+            //user is an employee of the store, he gets a 30% discount 
+            
+            if (user.Role == Role.EMPLOYEE)
+            {
+                discountPercentage = DiscountSettingHelper.EmployeeDiscount;
+            }
+            //user is an affiliate of the store, he gets a 10% discount 
+            else if (user.Role == Role.AFFILATE)
+            {
+                discountPercentage = DiscountSettingHelper.AffilateDiscount;
+            }
+            //user has been a customer for over 2 years, he gets a 5 % discount
+            else if (user.Role == Role.CUSTOMER && IsUserEllgibleForLoyaltyDiscount(user))
+            {
+                discountPercentage = DiscountSettingHelper.LoyelityDiscount;
+            }
+
+            //Total Percentage discount
+            var productTotalExceptGrocery = products.Where(p => p.Category == Category.GROCERY);
+            decimal totalPercentileDiscount = (discountPercentage / 100) * productTotalExceptGrocery.Sum(x => x.Price);
+
+
+            //For every $100 on the bill, there would be a $ 5 discount (e.g. for $ 990, you get $ 45 as a discount). 
+            int numberOfHundredMultiply = (int)(products.Sum(x => x.Price) / DiscountSettingHelper.DiscountOnSaleVolume);
+            decimal discountOnEachHundred = numberOfHundredMultiply * DiscountSettingHelper.SaleVolumeDiscount;
+
+            return new DiscountModel
+            {
+                PercentileDiscount = totalPercentileDiscount,
+                TotalAmountDiscount = discountOnEachHundred
+            };
+
         }
-         
+
+        public static bool IsUserEllgibleForLoyaltyDiscount(User user)
+        {
+            return (user.CreateDate.Year - DateTime.Now.Year) > DiscountSettingHelper.LoyaltyEligibilityYear;
+        }
     }
+
 
 }
